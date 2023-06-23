a6abe187eba45058fb6994f02e9ecc0458985cf4
Banky <kingmorty@protonmail.com>
2023-06-23 13:35:06 +0100

    Update file structure

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple homepage I designed with Tailwind CSS, modelled after [this](https://iteck-html.themescamp.com/home-app-landing.html) bootstrap template.
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: ["./assets/**/*.{html,js}"],
+  },
+  theme: {
+    extend: {
+      backgroundImage: {
+        "header-texture": "url('/assets/images/top_bg.png')",
+        "hamburger-svg": "url('/assets/svg/hamburger.svg')",
+      },
+      fontSize: {
+        xxs: "0.5rem",
+      },
+      transitionProperty: {
+        height: "height",
+        "max-height": "max-height",
+        spacing: "margin, padding",
+      },
+      colors: {
+        grayish: "#f0eff5",
+      },
+    },
+  },
+  plugins: [],
+};
